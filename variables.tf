ce529ac317f59b501a8137d4800189f3fa4063a1
Jeff Sani <jeff.sani@gmail.com>
2026-07-30 23:27:39 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,10 @@ variable "cloudflare_account_id" { type = string }
 variable "cloudflare_anycast_ip_1" { type = string }
 variable "cloudflare_anycast_ip_2" { type = string }
 variable "ubuntu_wan_ip" { type = string }
-
+variable "cloudflare_api_token" {
+  type      = string
+  sensitive = true
+}
 variable "ssh_user" { 
   type    = string
   default = "terraform" 
