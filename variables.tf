69832c227b92b320fbffdd0200e8d770eb20beb0
Jeff Sani <jeff.sani@gmail.com>
2026-07-30 23:16:14 -0700

    Refactor health check target variables for tunnels

--- variables.tf
+++ variables.tf
@@ -17,16 +17,20 @@ variable "tunnel_1_interface_address" {
   type        = string
   description = "The Cloudflare side of the /31 CIDR block for Tunnel 1"
 }
-variable "tunnel_1_health_check_target" { 
-  type        = string
-  description = "The Customer side of the /31 CIDR block for Tunnel 1"
-}
 
 variable "tunnel_2_interface_address" { 
   type        = string
   description = "The Cloudflare side of the /31 CIDR block for Tunnel 2"
 }
+
+variable "tunnel_1_health_check_target" { 
+  type        = string
+  description = "The Customer side health check target. Defaults to ubuntu_wan_ip if omitted."
+  default     = null
+}
+
 variable "tunnel_2_health_check_target" { 
   type        = string
-  description = "The Customer side of the /31 CIDR block for Tunnel 2"
+  description = "The Customer side health check target. Defaults to ubuntu_wan_ip if omitted."
+  default     = null
 }
