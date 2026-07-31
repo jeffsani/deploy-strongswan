afd03fb679c50814ea5e10c5282c2095271b7f37
Jeff Sani <jeff.sani@gmail.com>
2026-07-31 16:49:31 -0700

    Remove tunnel_custom_fqdns output
    Removed output for custom FQDNs from outputs.tf.

--- outputs.tf
+++ outputs.tf
@@ -4,11 +4,6 @@ output "tunnel_generated_psks" {
   sensitive   = true
 }
 
-output "tunnel_custom_fqdns" {
-  description = "The discrete Custom FQDNs applied to the IPsec tunnels"
-  value       = local.custom_fqdns
-}
-
 output "tunnel_interface_addresses" {
   description = "The calculated VTI interface addresses"
   value       = cloudflare_magic_wan_ipsec_tunnel.tunnels[*].interface_address
