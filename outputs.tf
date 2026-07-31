dbc1f607ca6a67be096b1a42ee61e2267ebb1b08
Jeff Sani <jeff.sani@gmail.com>
2026-07-31 15:16:01 -0700

    Refactor IPsec tunnel outputs for dynamic keys and IDs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
-output "tunnel_1_generated_psk" {
-  description = "The auto-generated PSK for the first IPsec tunnel"
-  value       = cloudflare_magic_wan_ipsec_tunnel.tunnel_1.psk
+output "tunnel_generated_psks" {
+  description = "The dynamically generated Pre-Shared Keys for the IPsec tunnels"
+  value       = cloudflare_magic_wan_ipsec_tunnel.tunnels[*].psk
   sensitive   = true
 }
 
-output "tunnel_2_generated_psk" {
-  description = "The auto-generated PSK for the second IPsec tunnel"
-  value       = cloudflare_magic_wan_ipsec_tunnel.tunnel_2.psk
-  sensitive   = true
+output "tunnel_fqdn_ids" {
+  description = "The Cloudflare auto-generated FQDN IDs for the IPsec tunnels"
+  value       = cloudflare_magic_wan_ipsec_tunnel.tunnels[*].fqdn_id
+}
+
+output "tunnel_interface_addresses" {
+  description = "The calculated VTI interface addresses"
+  value       = cloudflare_magic_wan_ipsec_tunnel.tunnels[*].interface_address
 }
